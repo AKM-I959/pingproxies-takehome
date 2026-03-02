9b3637eb20e1cb3ceccd143e9c2eea6b1975df8c
Aidan McKellar <aidan.k.mckellar@gmail.com>
2026-03-02 16:57:05 +0000

    Improved auth endpoint username parsing.

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -26,19 +26,76 @@ def auth_user(request: User, db: Session = Depends(get_db)):
 
     #1. Parse request
 
+    #e.g alice residential c    gb  city london
+    #e.g alice residential c    us
+    #e.g alice residential city new york
+
+    #From what I can see in the docs the username is always first in the examples.
+    #Split by the second underscore.
+
+    #No valid country ISO codes should be longer than 1 word i.e no underscore.
+    #Cities we need to append all parts after the city word until either the end of the input
+    #OR
+    #Until we hit the c keyword to indicate a country.
+
     userParts = request.username.split("_")
-    #Username is always either 6 or 7 parts (depending on city e.g. new_york vs london)
-    if len(userParts) < 6 or len(userParts) > 7:
+
+    if len(userParts) < 2:
         return JSONResponse(
-            status_code=401,
-            content={"internal_error_code": 1003, "error_message": "invalid username"}
+            status_code=400, 
+            content={
+                "internal_error_code": 1007,
+                "error_message": "Invalid username."}
         )
-    
+
     name = f"{userParts[0]}_{userParts[1]}"
-    country = userParts[3]
-    city = userParts[5]
-    if len(userParts) == 7:
-        city += f"_{userParts[6]}"
+
+    country = None
+    city = None
+    cityParts = []
+
+    totParts = len(userParts)
+
+    for i in range(2, totParts):
+
+        if userParts[i] == "c" and (i+1) < totParts:
+            if userParts[i+1] == "c":
+                return JSONResponse(
+                    status_code=400, 
+                    content={
+                        "internal_error_code": 1006,
+                        "error_message": "Invalid username."}
+                )
+            if userParts[i+1] == "city":
+                return JSONResponse(
+                    status_code=400, 
+                    content={
+                        "internal_error_code": 1005,
+                        "error_message": "Invalid username."}
+                )
+            country = userParts[i+1]
+
+        elif userParts[i] == "city" and i + 1 < totParts:
+            for j in range(i+1, totParts):
+                if userParts[j] == "city":
+                    return JSONResponse(
+                        status_code=400, 
+                        content={
+                            "internal_error_code": 1004,
+                            "error_message": "Invalid username."}
+                    )
+                if userParts[j] == "c":
+                    if not cityParts:
+                        return JSONResponse(
+                            status_code=400, 
+                            content={
+                                "internal_error_code": 1003,
+                                "error_message": "Invalid username."}
+                        )
+                    break
+                cityParts.append(userParts[j])
+    if cityParts:
+        city = "_".join(cityParts)
 
     #2. Query DB
 
@@ -52,12 +109,16 @@ def auth_user(request: User, db: Session = Depends(get_db)):
     if not user:
         return JSONResponse(
             status_code=401,
-            content={"internal_error_code": 1002, "error_message": "user not found"}
+            content={
+                "internal_error_code": 1002,
+                "error_message": "User not found."}
         )
     if user.proxy_user_password != request.password:
         return JSONResponse(
             status_code=401,
-            content={"internal_error_code": 1001, "error_message": "invalid password"}
+            content={
+                "internal_error_code": 1001,
+                "error_message": "Invalid password."}
         )
     
     #4. If it passes the checks then send the formatted context
@@ -80,5 +141,3 @@ def auth_user(request: User, db: Session = Depends(get_db)):
             "available_bandwidth": bandwidth
         }
     )
-    
-    
